afba19fd604ad1f316c0e2af4b6ee8ba5da0514f
Freddy Phoenix Mills <118795606+anonphoenix007@users.noreply.github.com>
2024-05-03 05:58:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const bot = require(__dirname + '/lib/tmd')
+const bot = require(__dirname + '/lib/amd')
 const { VERSION } = require(__dirname + '/config')
 
 const start = async () => {
-    Debug.info(`Starting Raganork-bot-md ${VERSION}`)
+    Debug.info(`Starting Ragnarok-MD ${VERSION}`)
   try {
     await bot.init()
     //bot.logger.info('⏳ Database syncing!')
