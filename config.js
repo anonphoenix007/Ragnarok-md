378ff002194979b56dbb0a1735e3f8383cebdd4b
Freddy Phoenix Mills <118795606+anonphoenix007@users.noreply.github.com>
2024-05-03 05:09:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,20 +6,20 @@ global.audio = "";
 global.video = "";
 global.port = process.env.PORT;
 global.appUrl = process.env.APP_URL || "";
-global.email = "astromedia0010@gmail.com";
-global.location = "Lahore,Pakistan.";
+global.email = "phoenixgibson007@gmail.com";
+global.location = "Lagos,Nigeria";
 global.mongodb = process.env.MONGODB_URI || "";
 global.allowJids = process.env.ALLOW_JID || "null";
 global.blockJids = process.env.BLOCK_JID || "null";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
 global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Lagos";
-global.github = process.env.GITHUB || "https://github.com/Astropeda/Asta-Md";
-global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaPGt3QEwEjpBXT4Rv0z";
-global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaPGt3QEwEjpBXT4Rv0z";
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://i.imgur.com/P37NWrz.jpeg";
-global.devs = "2348039607375";
-global.sudo = process.env.SUDO || "2348039607375";
-global.owner = process.env.OWNER_NUMBER || "2348039607375";
+global.github = process.env.GITHUB || "https://github.com/anonphoenix007/Ragnarok-Md";
+global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaY0Zq32P59piTo5rg0K";
+global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaY0Zq32P59piTo5rg0K";
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/d2c61eac67addb31507fa.jpg";
+global.devs = "2347080968564";
+global.sudo = process.env.SUDO || "2347080968564";
+global.owner = process.env.OWNER_NUMBER || "2347080968564";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -27,8 +27,8 @@ global.warncount = process.env.WARN_COUNT || 3;
 global.disablepm = process.env.DISABLE_PM || "false";
 global.disablegroup = process.env.DISABLE_GROUPS || "false",
 global.MsgsInLog = process.env.MSGS_IN_LOG || "true";
-global.userImages = process.env.USER_IMAGES || "";
-global.waPresence = process.env.WAPRESENCE || "online";
+global.userImages = process.env.USER_IMAGES || "https://telegra.ph/file/d2c61eac67addb31507fa.jpg";
+global.waPresence = process.env.WAPRESENCE || "recording";//online,recording,composing,available,unavailable
 global.readcmds = process.env.READ_COMMAND || "false";
 global.readmessage = process.env.READ_MESSAGE || "false";
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
@@ -45,14 +45,14 @@ global.SESSION_ID =
   ""
 module.exports = {
   menu: process.env.MENU || "",
-  HANDLERS: process.env.PREFIX || "/",
+  HANDLERS: process.env.PREFIX || '"',
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "©ᴀsᴛᴀ ᴍᴅ",
-  author: process.env.PACK_AUTHER || "ᴀsᴛᴀ ᴍᴅ",
-  packname: process.env.PACK_NAME || "♥️",
-  botname: process.env.BOT_NAME || "ᴀsᴛᴀ ᴍᴅ",
-  ownername: process.env.OWNER_NAME || "ᴀsᴛᴀ",
+  caption: process.env.CAPTION || "©ʀᴀɢɴᴀʀᴏᴋ-ᴍᴅ",
+  author: process.env.PACK_AUTHER || "©ʀᴀɢɴᴀʀᴏᴋ-ᴍᴅ",
+  packname: process.env.PACK_NAME || "Tᴀɪʀᴀ•Mᴀᴋɪɴᴏ",
+  botname: process.env.BOT_NAME || "ʀᴀɢɴᴀʀᴏᴋ-ᴍᴅ",
+  ownername: process.env.OWNER_NAME || "Tᴀɪʀᴀ Mᴀᴋɪɴᴏ",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
@@ -64,7 +64,7 @@ module.exports = {
   aitts_Voice_Id: process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
   WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
-  LANG: (process.env.THEME || "ASTA").toUpperCase(),
+  LANG: (process.env.THEME || "RAGNAROK").toUpperCase(),
 };
 global.rank = "updated";
 global.isMongodb = false;
